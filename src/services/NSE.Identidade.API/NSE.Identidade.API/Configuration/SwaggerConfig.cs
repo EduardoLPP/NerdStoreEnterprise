6efd9b4b08640e0f316dae4b9974d8346e8aba69
eduardopasso <eduardo.pereira@tsea.com.br>
2023-09-05 15:27:22 -0300

    refatoracao e melhorias

---
+++ src/services/NSE.Identidade.API/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise API",
+                    Description = "API de estudo de ASP.NET Core",
+                    Contact = new OpenApiContact() { Name = "Eduardo Leonardo", Email = "eduardoleonardo15@gmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/MIT/") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
