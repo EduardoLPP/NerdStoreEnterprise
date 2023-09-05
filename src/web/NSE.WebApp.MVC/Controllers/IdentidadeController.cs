fe10eb504d9a96a92f03bf64a48f22b73c255fe0
eduardopasso <eduardo.pereira@tsea.com.br>
2023-09-05 18:23:29 -0300

    models views e controllers de login

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistroViewModel usuarioRegistro)
+        {
+             if(!ModelState.IsValid)
+                return View(usuarioRegistro);
+
+             //API - Registrar o usuário
+
+             if(false)
+                return View(usuarioRegistro);
+
+             //Realizar login na aplicação
+
+             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLoginViewModel usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioLogin);
+
+            //API - Login
+
+            if (false)
+                return View(usuarioLogin);
+
+            //Realizar login na aplicação
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
