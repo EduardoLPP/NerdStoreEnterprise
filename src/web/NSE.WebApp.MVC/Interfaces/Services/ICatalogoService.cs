9d097a3cfce08a2235784095572484c3fc8413b0
eduardopasso <eduardo.pereira@tsea.com.br>
2023-11-22 11:35:19 -0300

    catalogo-httpservice

---
+++ src/web/NSE.WebApp.MVC/Interfaces/Services/ICatalogoService.cs
@@ -0,0 +1,13 @@
+﻿using NSE.WebApp.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Intefaces
+{
+    public interface ICatalogoService
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+    }
+}
